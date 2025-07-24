ac051b0cca45b9612740bd0524794e58edc469b5
sti <86592209+sti-233@users.noreply.github.com>
2025-07-24 17:31:10 +0800

    fix: patch onFeedClick

---
+++ patches/src/main/kotlin/app/revanced/patches/bilibili/misc/json/fingerprints/CardClickProcessorNew3Fingerprint.kt
@@ -0,0 +1,20 @@
+package app.revanced.patches.bilibili.misc.json.fingerprints
+
+import app.revanced.patcher.fingerprint.MethodFingerprint
+
+object CardClickProcessorNew3Fingerprint : MethodFingerprint(
+    strings = listOf("tm.recommend.0.0"),
+    returnType = "V",
+    customFingerprint = { methodDef, _ ->
+        methodDef.parameterTypes.let {
+            it.size == 8
+            && it[1] == "Lcom/bilibili/pegasus/data/FeedbackType;"
+            && it[2] == "Lcom/bilibili/pegasus/data/base/BasePegasusData;"
+            && it[3] == "Lcom/bilibili/app/comm/list/common/data/DislikeReason;"
+            && it[4] == "Ljava/lang/String;"
+            && it[5] == "Z"
+            && it[6] == "Z"
+            && it[7] == "Z"
+        }
+    }
+)
